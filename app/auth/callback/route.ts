bfcc2773998643dc8d250220979f12dbb0be88cd
apastrana <apastrana6@gatech.edu>
2025-02-23 10:06:54 -0500

    auth bug fix

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,16 +1,28 @@
-import { createRouteHandlerClient } from '@supabase/ssr'
-import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
+import { type EmailOtpType } from '@supabase/supabase-js'
+import { type NextRequest } from 'next/server'
 
-export async function GET(request: Request) {
-  const requestUrl = new URL(request.url)
-  const code = requestUrl.searchParams.get('code')
+import { createClient } from '@/utils/supabase/server'
+import { redirect } from 'next/navigation'
 
-  if (code) {
-    const cookieStore = cookies()
-    const supabase = createRouteHandlerClient({ cookies: () => cookieStore })
-    await supabase.auth.exchangeCodeForSession(code)
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url)
+  const token_hash = searchParams.get('token_hash')
+  const type = searchParams.get('type') as EmailOtpType | null
+  const next = searchParams.get('next') ?? '/'
+
+  if (token_hash && type) {
+    const supabase = await createClient()
+
+    const { error } = await supabase.auth.verifyOtp({
+      type,
+      token_hash,
+    })
+    if (!error) {
+      // redirect user to specified redirect URL or root of app
+      redirect(next)
+    }
   }
 
-  return NextResponse.redirect(`${requestUrl.origin}/dashboard`)
+  // redirect the user to an error page with some instructions
+  redirect('/error')
 }
